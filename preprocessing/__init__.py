80f1459cc151b4518d8bc4f5e2c2c6eeb7032363
GiuliaLanzillotta <lanzillottagiulia97@gmail.com>
2020-04-10 09:32:34 +0200

    Added a structure to the project

---
+++ preprocessing/__init__.py
@@ -0,0 +1,2 @@
+train_positive_location = '../data/twitter-datasets/train_pos.txt'
+train_negative_location = '../data/twitter-datasets/train_neg.txt'
---
+++ preprocessing/preprocess.py
@@ -0,0 +1,5 @@
+from . import train_negative_location,train_negative_location
+
+
+if __name__ == "__main__":
+    pass
