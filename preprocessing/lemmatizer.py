1fa6cc9f8bac55e18ff43dae38c2e6cb75cff682
Francesco Ignazio Re <francescoignazio97@gmail.com>
2020-04-16 20:37:27 +0200

    Update lemmatizer.py

--- preprocessing/lemmatizer.py
+++ preprocessing/lemmatizer.py
@@ -92,3 +92,26 @@ def  TxtLemmatized(file_name = "train_pos.txt", stopword = 0, output_file = "lem
         f.writelines(file_new)
     return file_new
       
+
+replacement_patterns = [
+(r'won\'t', 'will not'),
+(r'can\'t', 'cannot'),
+(r'i\'m', 'i am'),
+(r'ain\'t', 'is not'),
+(r'(\w+)\'ll', '\g<1> will'),
+(r'(\w+)n\'t', '\g<1> not'),
+(r'(\w+)\'ve', '\g<1> have'),
+(r'(\w+)\'s', '\g<1> is'),
+(r'(\w+)\'re', '\g<1> are'),
+(r'(\w+)\'d', '\g<1> would')
+]
+
+#Class that enables to change a string transforming grammatical forms like don't and can't in do not and cannot.
+class RegexpReplacer(object):
+    def __init__(self, patterns=replacement_patterns):
+        self.patterns = [(re.compile(regex), repl) for (regex, repl) in patterns]
+    def replace(self, text):
+        s = text
+        for (pattern, repl) in self.patterns:
+            s = re.sub(pattern, repl, s)
+        return s
