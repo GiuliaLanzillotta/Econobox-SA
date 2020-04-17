03f1dca5dffbed5abc688ad696aa3311d3cf0cfb
GiuliaLanzillotta <lanzillottagiulia97@gmail.com>
2020-04-17 09:45:06 +0200

    skeleton of pipeline updated

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -1,2 +1,13 @@
 #pipeline methods for preprocessing
 
+def get_vocabulary():
+    pass
+
+def get_lemmatization():
+    pass
+
+def get_cooc_matrix():
+    pass
+
+def run_preprocessing():
+    pass
