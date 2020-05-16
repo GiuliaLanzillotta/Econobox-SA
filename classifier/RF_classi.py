c4841590568436b84f8505701cbd7d051c10b6c3
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-05-16 17:57:00 +0200

    Add files via upload

---
+++ classifier/RF_classi.py
@@ -0,0 +1,80 @@
+from classifier.classifier_base import ClassifierBase
+from classifier import models_store_path
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
+
+class RF_classi(ClassifierBase):
+    """Random Forest classifier"""
+
+    def __init__(self,
+                 embedding_dimension,
+                 name="ourRF"):
+        super().__init__(embedding_dimension, name=name)
+        self.history = None
+        self.model = None
+
+    def build(self, **kwargs):
+        print("Building model.")
+
+        n_estimators = kwargs.get("n_estimators")
+        criterion = kwargs.get("criterion")
+        max_depth = kwargs.get("max_depth")
+        min_samples_split = kwargs.get("min_sample_split")
+        min_samples_leaf = kwargs.get("min_samples_leaf")
+        min_weight_fraction_leaf = kwargs.get("min_weight_fraction_leaf")
+        max_features = kwargs.get("max_features")
+        bootstrap = kwargs.get("bootstrap")
+        warm_start = kwargs.get("warm_start")
+        class_weight = kwargs.get("class_weight")
+
+        if not n_estimators: n_estimators = 100
+        if not criterion: criterion = "gini"
+        if not max_depth: max_depth = None
+        if not min_samples_split: min_samples_split = 2
+        if not min_samples_leaf: min_samples_leaf = 1
+        if not min_weight_fraction_leaf : min_weight_fraction_leaf = 0.0
+        if not max_features : max_features = "auto"
+        if not bootstrap : bootstrap = True
+        if not warm_start : warm_start = False
+
+        self.model = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth,
+                                            min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf,
+                                            min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features,
+                                            bootstrap=bootstrap, warm_start=warm_start)
+
+    def train(self, x, y, **kwargs):
+        """
+        Training the model and saving the history of training
+        """
+        print("Training model")
+        self.history = self.model.fit(x,y)
+        crosvalscore = cross_val_score(self.model, x, y, cv=5)
+        print(crosvalscore)
+
+    def test(self, x,y, **kwargs):
+        print("Testing model")
+        y_pred = self.model.predict(x)
+        score = f1_score(y, y_pred)
+        return(score)
+
+    def make_predictions(self, x, save=True, **kwargs):
+        print("Making predictions")
+        preds = self.model.predict(x)
+        preds_classes = np.argmax(preds, axis=-1).astype("int")
+        preds_classes[preds_classes == 0] = -1
+        if save: self.save_predictions(preds_classes)
+
+    def save(self, overwrite=True, **kwargs):
+        print("Saving model")
+        path = models_store_path+self.name
+        pickle.dump(self, open(path, 'wb'))
+        joblib.dump(self, 'ourRF.pkl')
+
+    def load(self, **kwargs):
+        print("Loading model")
+        path = models_store_path+self.name
+        self.model = joblib.load('ourRF.pkl')
