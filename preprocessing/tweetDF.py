5f9b35beb8d8ca2e7e01642973f8339646d70ed8
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-06-12 00:33:52 +0200

    Add files via upload

---
+++ preprocessing/tweetDF.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from data import train_positive_location, train_negative_location, \
+  train_positive_sample_location, train_negative_sample_location
+import pickle
+
+def get_tweet_df():
+    df_pos = pd.read_table(train_positive_sample_location, names=('tweet', 'id'))
+    df_neg = pd.read_table(train_negative_sample_location, names=('tweet', 'id'))
+    df_pos_1 = pd.DataFrame({
+        'text': df_pos['tweet'],
+        'sent': 0
+    })
+
+    df_neg_1 = pd.DataFrame({
+        'text': df_neg['tweet'],
+        'sent': 1
+    })
+    df_data = pd.concat([df_pos_1, df_neg_1], ignore_index=True, sort=False)
+
+    with open("../data/tweetDF.pkl", 'wb') as f:
+        pickle.dump(df_data,f, pickle.HIGHEST_PROTOCOL)
+
+def load_tweetDF():
+  with open("../data/tweetDF.pkl", 'rb') as f:
+    tweetDF = pickle.load(f)
+  return tweetDF
+
