97fc01f2157dbeb6ca189f3278a23092e1100576
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-06-12 17:02:47 +0200

    Add files via upload

--- classifier/main.py
+++ classifier/main.py
@@ -3,42 +3,29 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(ROOT_DIR)
 from embedding import zero_matrix_train_location, zero_matrix_test_location, \
-    zero_matrix_full_train_location, replaced_zero_matrix_full_train_location, \
-    replaced_zero_matrix_test_location
+    zero_matrix_full_train_location, replaced_zero_matrix_full_train_location
+from data import tweetDF_location
 from classifier.pipeline import run_train_pipeline
 
 
 
 if __name__ == "__main__":
 
-    build_params = {
-        "cell_type":"GRU",
-        "num_layers":1,
-        "hidden_size":64,
-        "optimizer":"adam",
-        "dropout_rate":0.4,
-        "train_embedding":False,
-        "use_normalization":True,
-        "use_attention":True,
-        "heads":5,
-        "penalization":False,
-        "gamma":0.3,
-        "use_convolution":False,
-        "dilation_rate":1
-    }
-    train_params = {"epochs":10,
-                    "batch_size":1024,
+    build_params = None
+    train_params = {"epochs":3,
+                    "batch_size":64,
                     "validation_split":0.2}
 
-    run_train_pipeline("recurrent_NN",
-                       "Attention_GRU_5heads_full",
+    run_train_pipeline("BERT_NN",
+                       "BERT_NN_1st",
                        load_model=False,
                        prediction_mode=False,
-                       data_location=replaced_zero_matrix_full_train_location,
+                       text_data_mode_on=True,
+                       data_location=tweetDF_location,
+                       max_seq_length=128,
                        cv_on=False,
-                       choose_randomly=True,
-                       random_percentage=0.1,
                        test_data_location=None,
                        build_params = build_params,
                        train_params=train_params,
                        generator_mode=False)
+
