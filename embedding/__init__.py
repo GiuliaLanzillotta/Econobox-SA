b2c872731c2dae294638043f9e790aadc0030339
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-06-21 21:55:29 +0200

    Update __init__.py

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -10,6 +10,7 @@ zero_matrix_test_location = "../zero_embedding_test_matrix.npz"
 replaced_zero_matrix_test_location = "../replaced_zero_embedding_test_matrix.npz"
 stanford_embedding_location = "../embeddings/glove.twitter.27B.200d.txt"
 bert_matrix_train_location = "../bert_embedding_train_matrix.npz"
-bert_matrix_train_location2 = "../bert_embedding_trani_matrix2.npz"
+bert_matrix_train_location2 = "../bert_embedding_train_matrix2.npz"
+roberta_matrix_train_location = "../roberta_embedding_train_matrix2.npz"
 embedding_dim = 200
 
