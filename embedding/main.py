d7865cdefc615d62ba5c23a208cf9710af75f5ad
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-01 00:06:34 +0200

    Add files via upload

--- embedding/main.py
+++ embedding/main.py
@@ -10,13 +10,13 @@ import embedding
 import data
 
 if __name__ == "__main__":
-    input_files = [data.replaced_train_full_negative_location,
-                   data.replaced_train_full_positive_location]
-    run_embedding_pipeline(embedding_fun="transformer_emb",
-                           prediction_mode=False,
+    input_files = [data.train_negative_location,
+                   data.train_positive_location]
+    run_embedding_pipeline(embedding_fun="sum_embeddings",
+                           prediction_mode=True,
                            input_entries=data.full_dimension,
                            input_files=input_files,
-                           output_location=embedding.roberta_full_matrix_train_location,
+                           output_location=embedding.matrix_test_location2,
                            embedding="roberta-base",
-                           max_len=768) # 768 for roberta, 50 for the no embedding function
+                           max_len=200) # 768 for roberta, 50 for the no embedding function, 200 Glove
     exit(0)
