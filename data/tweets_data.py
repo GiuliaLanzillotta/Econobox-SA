e9dfdb74493545de2c195dae8ebf34d174a0d43f
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-12 15:17:35 +0200

    Add files via upload

--- data/tweets_data.py
+++ data/tweets_data.py
@@ -30,19 +30,25 @@ import tensorflow_datasets as tfds
 from preprocessing import standard_vocab_name
 from preprocessing.pipeline import get_vocabulary
 from data import input_files_location
+from data import train_negative_sample_location, train_positive_sample_location
+from data import train_positive_location, train_negative_location
 import numpy as np
 import os
 
+
 class TweetDataset():
     "Wrapper class for tf.data.Dataset object"
+
     def __init__(self, input_files,
                  labels=None,
-                 batch_size=128,
+                 batch_size=2048,
                  buffer_size=1000,
                  vocabulary=None,
                  max_len=100,
-                 validation_size=1000,
-                 encode_text=False):
+                 validation_size=10000,
+                 test_size=10000,
+                 encode_text=False,
+                 do_padding=False):
         """
         :param encode_text: (bool)
         :param max_len: maximum length of the input sequence
@@ -66,19 +72,22 @@ class TweetDataset():
         self.buffer_size = buffer_size
         self.labels = labels
         self.dataset = self.load_dataset(labels)
+        self.do_padding = do_padding
         if encode_text:
-            if not vocabulary: vocabulary=standard_vocab_name
+            if not vocabulary: vocabulary = standard_vocab_name
             self.vocab = get_vocabulary(vocabulary)
             self.encode_text(self.vocab)
         if self.labels:
-            train, validate = self.split_dataset(batch_size, validation_size, max_len)
+            train, validate, test = self.split_dataset(batch_size, validation_size, test_size, max_len)
             print("Caching.")
             abs_path = os.path.abspath(os.path.dirname(__file__))
-            paths = [os.path.join(abs_path, f) for f in ["../data/train_data", "../data/valid_data"]]
+            paths = [os.path.join(abs_path, f) for f in
+                     ["../data/train_data", "../data/valid_data", "../data/test_data"]]
             self.train = train.cache(paths[0])
             self.validate = validate.cache(paths[1])
-        else: self.dataset = self.dataset.padded_batch(batch_size, padded_shapes=[max_len])
-
+            self.test = test.cache(paths[2])
+        else:
+            self.dataset = self.dataset.padded_batch(batch_size, padded_shapes=[max_len])
 
     def load_dataset(self, labels):
         """
@@ -91,21 +100,30 @@ class TweetDataset():
         print("Loading the dataset.")
         ## READING TEXT FILES
         abs_path = os.path.abspath(os.path.dirname(__file__))
-        paths = [os.path.join(abs_path,f) for f in self.input_files]
+        paths = [os.path.join(abs_path, f) for f in self.input_files]
         datasets = []
         for path in paths:
             datasets.append(tf.data.TextLineDataset(path))
+            #print(type(tf.data.TextLineDataset(path)))
         ## ADDING LABELS (if requested)
         if labels:
             assert len(labels) == len(self.input_files)
             for i in range(len(datasets)):
-                datasets[i] = datasets[i].map(lambda line: (line,tf.one_hot(labels[i],2)))
+                datasets[i] = datasets[i].map(lambda line: (tf.expand_dims(line, axis=0), tf.expand_dims(tf.one_hot(labels[i], 2), axis=0)))
         ##MERGING the datasets
         dataset_final = datasets[0]
-        for dataset in datasets:
-            dataset_final = dataset_final.concatenate(dataset)
+        #for dataset in datasets:
+        #    print(dataset)
+        #    dataset_final = dataset_final.concatenate(dataset)
+        #dataset_final = datasets[0]
+        dataset_final = dataset_final.concatenate(datasets[1])
         ##SHUFFLE
         dataset_final = dataset_final.shuffle(self.buffer_size, reshuffle_each_iteration=False)
+        #for num, _ in enumerate(dataset_final):
+        #    pass
+
+        #print(f'Number of elements: {num}')
+
         return dataset_final
 
     def encode_text(self, vocabulary):
@@ -113,6 +131,7 @@ class TweetDataset():
         print("Encoding the dataset.")
         vocabulary_set = set(vocabulary.keys())
         encoder = tfds.features.text.TokenTextEncoder(vocabulary_set)
+
         # We want to use Dataset.map to apply this function to each element
         # of the dataset. Dataset.map runs in graph mode so you can't .map
         # this function directly: You need to wrap it in a tf.py_function.
@@ -127,30 +146,52 @@ class TweetDataset():
         # in a once-differentiable TensorFlow operation that executes
         # it with eager execution enabled.
         def encode_map_fn(text, label):
-            encoded_text, label = tf.py_function(lambda t,l: (encoder.encode(t.numpy()),l),
+            encoded_text, label = tf.py_function(lambda t, l: (encoder.encode(t.numpy()), l),
                                                  inp=[text, label], Tout=(tf.int64, tf.float32))
             encoded_text.set_shape([None])
             label.set_shape([])
             return encoded_text, label
+
         def encode_map_fn_no_label(text):
             encoded_text, label = tf.py_function(lambda t: encoder.encode(t.numpy()),
-                                                 inp=[text],Tout=tf.int64)
+                                                 inp=[text], Tout=tf.int64)
             encoded_text.set_shape([None])
             label.set_shape([])
             return encoded_text
-        # finally applying the transformation word->idx on the dataset
-        if self.labels: self.dataset = self.dataset.map(encode_map_fn)
-        else: self.dataset = self.dataset.map(encode_map_fn_no_label)
 
-    def split_dataset(self, batch_size, validation_size, max_len):
+        # finally applying the transformation word->idx on the dataset
+        if self.labels:
+            self.dataset = self.dataset.map(encode_map_fn)
+        else:
+            self.dataset = self.dataset.map(encode_map_fn_no_label)
+
+    def split_dataset(self, batch_size, validation_size, test_size, max_len):
         """Splits the dataset in training and validation."""
         print("Splitting dataset.")
-        train_data = self.dataset.skip(validation_size).shuffle(self.buffer_size)
-        train_data = train_data.padded_batch(batch_size, padded_shapes=([max_len], []))
+        train_data = self.dataset.skip(validation_size + test_size).shuffle(self.buffer_size)
+        #for num, _ in enumerate(train_data):
+        #    pass
+
+        #print(f'Number of elements: {num}')
+        if self.do_padding:
+            train_data = train_data.padded_batch(batch_size, padded_shapes=([max_len], [1]))
         validation_data = self.dataset.take(validation_size)
-        validation_data = validation_data.padded_batch(batch_size, padded_shapes=([max_len], []))
-        return train_data.prefetch(self.buffer_size),\
-               validation_data.prefetch(self.buffer_size)
+        if self.do_padding:
+            validation_data = validation_data.padded_batch(batch_size, padded_shapes=([max_len], [1]))
+        test_data = self.dataset.skip(validation_size).take(test_size)
+        if self.do_padding:
+            test_data = test_data.padded_batch(batch_size, padded_shapes=([max_len], [1]))
+        return train_data.prefetch(self.buffer_size), \
+               validation_data.prefetch(self.buffer_size), \
+               test_data.prefetch(self.buffer_size)
+
+
+#cool_dataset = TweetDataset(input_files=[train_negative_location, train_positive_location], labels=[0, 1],
+#                          encode_text=False, do_padding=False)
 
 
 
+#for num, _ in enumerate(cool_dataset.train):
+#    pass
+
+#print(f'Number of elements: {num}')
