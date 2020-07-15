6a4b7624dbb9ce4fc9723da606324ed21edff109
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-15 20:05:48 +0200

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -6,11 +6,16 @@ replaced_train_positive_location = '../data/twitter-datasets/replaced_train_pos.
 replaced_train_negative_location = '../data/twitter-datasets/replaced_train_neg.txt'
 replaced_train_full_positive_location = '../data/twitter-datasets/replaced_train_pos_full_no_duplicates.txt'
 replaced_train_full_negative_location = '../data/twitter-datasets/replaced_train_neg_full_no_duplicates.txt'
+
+replaced_train_full_positive_location_d = '../data/twitter-datasets/replaced_train_pos_full.txt'
+replaced_train_full_negative_location_d = '../data/twitter-datasets/replaced_train_neg_full.txt'
+
 test_location = "../data/twitter-datasets/test_data.txt"
 replaced_test_location = "../data/twitter-datasets/replaced_test_data_no_numbers.txt"
 input_files_location = "../data/twitter-datasets/"
 tweetDF_location = "../data/tweetDF.pkl"
 predDF_location = "../data/predDF.pkl"
+tf_idf_data_location = "../data/tf_idf_vectorized.pkl"
 
 bert_model_name='uncased_L-12_H-768_A-12'
 bert_vocab_location= '../data/uncased_L-12_H-768_A-12/vocab.txt'
@@ -19,4 +24,5 @@ bert_config_file_location = "../data/uncased_L-12_H-768_A-12/bert_config.json"
 
 sample_dimension = 200000
 full_dimension = 1250000*2
+half_full_dimension = 1250000
 test_dimension = 10000
