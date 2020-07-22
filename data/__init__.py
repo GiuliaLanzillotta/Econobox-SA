3fd4d9599fdc8afab4a4f5c4d55c842c75b0b528
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-22 07:26:34 +0200

    added locations

--- data/__init__.py
+++ data/__init__.py
@@ -11,6 +11,9 @@ train_matrices_folder = "../data/training_matrices/"
 replaced_train_full_positive_location_d = '../data/twitter-datasets/replaced_train_pos_full.txt'
 replaced_train_full_negative_location_d = '../data/twitter-datasets/replaced_train_neg_full.txt'
 
+replaced_train_full_positive_location_30 = "../data/twitter-datasets/replaced_train_neg_full_30.txt"
+replaced_train_full_negative_location_30 = "../data/twitter-datasets/replaced_train_pos_full_30.txt"
+
 test_location = "../data/twitter-datasets/test_data.txt"
 replaced_test_location = "../data/twitter-datasets/replaced_test_data_no_numbers.txt"
 input_files_location = "../data/twitter-datasets/"
@@ -27,3 +30,7 @@ sample_dimension = 200000
 full_dimension = 1250000*2
 half_full_dimension = 1250000
 test_dimension = 10000
+
+subset_thirty_percent_dimension_pos = 374964
+subset_thirty_percent_dimension_neg = 374265
+subset_thirty_percent_dimension = 749229
