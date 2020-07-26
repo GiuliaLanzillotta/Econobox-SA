3ffdd338e5916d759d13ff41eff8997a3b861c96
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-26 22:38:25 +0200

    Add files via upload

--- preprocessing/tweetDF.py
+++ preprocessing/tweetDF.py
@@ -9,39 +9,25 @@ from sklearn.utils import shuffle
 
 def get_tweet_df(input_files, random_percentage):
     abs_path = os.path.abspath(os.path.dirname(__file__))
-    df_pos = pd.read_table(os.path.join(abs_path, input_files[0]), names=('tweet', 'id'))
-    df_neg = pd.read_table(os.path.join(abs_path, input_files[1]), names=('tweet', 'id'))
-    df_pos_1 = pd.DataFrame({
-        'tweet': df_pos['tweet'],
-        'label': 0
-    })
-    df_pos_1_s = df_pos_1.sample(n=int(df_pos_1.shape[0]*random_percentage))
+    df_pos = pd.read_fwf(os.path.join(abs_path, input_files[0]), names=['tweet','label'])
+    df_neg = pd.read_fwf(os.path.join(abs_path, input_files[1]), names=['tweet', 'label'])
+    df_pos['label'] = 0
+    df_neg['label'] = 1
 
-    df_neg_1 = pd.DataFrame({
-        'tweet': df_neg['tweet'],
-        'label': 1
-    })
-    df_neg_1_s = df_neg_1.sample(n=int(df_neg_1.shape[0]*random_percentage))
-    df_data = pd.concat([df_pos_1_s, df_neg_1_s], ignore_index=True, sort=False)
+    df_pos_s = df_pos.sample(n=int(df_pos.shape[0]*random_percentage))
+    df_neg_s = df_neg.sample(n=int(df_neg.shape[0]*random_percentage))
+
+    df_data = pd.concat([df_pos_s, df_neg_s], ignore_index=True, sort=False)
     df_data = shuffle(df_data)
-    df_data = df_data[:50]
-    abs_path = os.path.abspath(os.path.dirname(__file__))
-    #with open(os.path.join(abs_path,"../data/tweetDF.pkl"), 'wb') as f:
-    #    pickle.dump(df_data,f, pickle.HIGHEST_PROTOCOL)
 
     return df_data
 
-def get_tweet_df_pred(input_files):
-    abs_path = os.path.abspath(os.path.dirname(__file__))
-    df_pred = pd.read_table(os.path.join(abs_path, input_files),names=('tweet', 'id'))
-    df_pred = pd.DataFrame({
-        'tweet':df_pred['tweet']
-    })
-    abs_path = os.path.abspath(os.path.dirname(__file__))
-    #with open(os.path.join(abs_path, "../data/predDF.pkl"), 'wb') as f:
-    #    pickle.dump(df_pred, f, pickle.HIGHEST_PROTOCOL)
-    return df_pred
 
+def get_tweet_df_pred(input_files):
+    print(input_files)
+    abs_path = os.path.abspath(os.path.dirname(__file__))
+    df_pred = pd.read_fwf(os.path.join(abs_path, input_files), names=['tweet', "nan1", "nan2"])
+    return df_pred
 
 def load_tweetDF():
     abs_path = os.path.abspath(os.path.dirname(__file__))
