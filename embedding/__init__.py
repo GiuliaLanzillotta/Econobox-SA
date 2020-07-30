e1b5b948caf52430d9c3238f7b78bf824133469a
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-31 00:11:38 +0200

    Update __init__.py

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -3,6 +3,7 @@ embeddings_folder = "../embeddings/"
 matrix_train_location = "../data/training_matrices/train_matrix.npz"
 matrix_test_location = "../data/training_matrices/test_matrix.npz"
 neg_sampling_matrix_train_location = "../data/neg_sampling_train.npz"
+
 zero_matrix_train_location = "../data/training_matrices/zero_embedding_train_matrix.npz"
 zero_matrix_full_train_location = "../data/training_matrices/zero_embedding_full_train_matrix.npz"
 replaced_zero_matrix_train_location = "../data/training_matrices/replaced_zero_embedding_train_matrix.npz"
@@ -10,9 +11,17 @@ replaced_zero_matrix_full_train_location = "../data/training_matrices/replaced_z
 zero_matrix_test_location = "../data/training_matrices/zero_embedding_test_matrix.npz"
 replaced_zero_matrix_test_location = "../data/training_matrices/replaced_zero_embedding_test_matrix.npz"
 stanford_embedding_location = "../embeddings/glove.twitter.27B.200d.txt"
-bert_matrix_train_location = "../data/training_matrices/bert_embedding_train_matrix.npz"
-bert_matrix_train_location2 = "../data/training_matrices/bert_embedding_train_matrix2.npz"
-roberta_matrix_train_location = "../data/training_matrices/roberta_embedding_train_matrix.npz"
-roberta_full_matrix_train_location = "../data/training_matrices/roberta_full_embedding_train_matrix.npz"
+
+glove_30_matrix_train_location = "../data/training_matrices/train_glove_30.npz"
+glove_30_matrix_train_location_tfidf = "../data/training_matrices/train_glove_30_tfidf.npz"
+glove_30_matrix_test_location = "../data/training_matrices/test_glove_30_try.npz"
+glove_30_matrix_test_location_2 = "../data/training_matrices/test_glove_30_2.npz"
+replaced_test_matrix_location = "../data/training_matrices/replaced_sample_matrix.npz"
+glove_30_matrix_train_location_new = '../data/training_matrices/train_glove_final.npz'
+glove_30_tfidf_sample_location = '../data/training_matrices/train_glove_sample_tfidf.npz'
+
+test_matrix_replaced_glove = '../data/training_matrices/test_matrix_replaced_glovesample.npz'
+
+
 embedding_dim = 200
 
