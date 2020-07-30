f1f6fedb4035791b8e7803791f3269bdb795e379
Charlotte Out <31860252+charlotteout@users.noreply.github.com>
2020-07-30 23:25:10 +0200

    Update __init__.py

--- classifier/__init__.py
+++ classifier/__init__.py
@@ -1,36 +1,3 @@
-train_positive_sample_location = '../data/twitter-datasets/train_pos.txt'
-train_negative_sample_location = '../data/twitter-datasets/train_neg.txt'
-train_positive_location = '../data/twitter-datasets/train_pos_full.txt'
-train_negative_location = '../data/twitter-datasets/train_neg_full.txt'
-replaced_train_positive_location = '../data/twitter-datasets/replaced_train_pos.txt'
-replaced_train_negative_location = '../data/twitter-datasets/replaced_train_neg.txt'
-replaced_train_full_positive_location = '../data/twitter-datasets/replaced_train_pos_full_no_duplicates.txt'
-replaced_train_full_negative_location = '../data/twitter-datasets/replaced_train_neg_full_no_duplicates.txt'
-train_matrices_folder = "../data/training_matrices/"
-
-replaced_train_full_positive_location_d = '../data/twitter-datasets/replaced_train_pos_full.txt'
-replaced_train_full_negative_location_d = '../data/twitter-datasets/replaced_train_neg_full.txt'
-
-replaced_train_full_positive_location_30 = "../data/twitter-datasets/replaced_train_neg_full_30.txt"
-replaced_train_full_negative_location_30 = "../data/twitter-datasets/replaced_train_pos_full_30.txt"
-
-test_location = "../data/twitter-datasets/test_data.txt"
-replaced_test_location = "../data/twitter-datasets/replaced_test_data_no_numbers.txt"
-input_files_location = "../data/twitter-datasets/"
-tweetDF_location = "../data/tweetDF.pkl"
-predDF_location = "../data/predDF.pkl"
-tf_idf_data_location = "../data/tf_idf_vectorized.pkl"
-
-bert_model_name='uncased_L-12_H-768_A-12'
-bert_vocab_location= '../data/uncased_L-12_H-768_A-12/vocab.txt'
-bert_ckpt_file_location = "../data/uncased_L-12_H-768_A-12/bert_model.ckpt"
-bert_config_file_location = "../data/uncased_L-12_H-768_A-12/bert_config.json"
-
-sample_dimension = 200000
-full_dimension = 1250000*2
-half_full_dimension = 1250000
-test_dimension = 10000
-
-subset_thirty_percent_dimension_pos = 374964
-subset_thirty_percent_dimension_neg = 374265
-subset_thirty_percent_dimension = 749229
+models_store_path = "../classifiers/" # base path to saving the models
+predictions_folder = "../data/"
+K_FOLD_SPLITS = 10
